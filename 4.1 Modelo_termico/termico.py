1b628b49840c830b94f71583b22f86777264ece1
nicol <nicol@DESKTOP-AAQBA66.local>
2020-08-20 23:54:28 -0300

    Update funciones_termico.py, termico.py, and 14 more files...

---
+++ 4.1 Modelo_termico/termico.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+############## Thermal Model Block ##########################
+  # Coded by Universitwin - Digital Twin Team
+  # April 2020
+  # Started by: Nicolás Valentín Conde
+  # Version: V02
+  # Latest version by:
+  # Latest version date:
+
+# Libraries...
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Functions...
+
+# Input
+
+# Input 1: Thermal Properties of materials
+
+# Input 2: Position, attitude, time
+
+# Input 3: Experiment configuration
+
+# Algorithm...
+
+# Output...
+
+# Visualization
+  # Input 1 visualization
+
+########## ##     ####     ######
+    ##     ##    ##  ##    ##   ##
+    ##     ##   ##    ##   ##   ##
+    ##     ##   ########   ######
+    ##     ##  ##      ##  ##   ## 
+    ##     ##  ##      ##  ##   ##
+    ##     ##  ##      ##  ######
---
+++ 4.4 Modelo_albedo/functions_albedo.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 20 22:02:13 2020
+
+@author: nicol
+"""
+
+
+def sum(x,y):
+    return x+y
+
+def average(x,y):
+    return (x+y)/2
+
+def power(x,y):
+    return x**y
